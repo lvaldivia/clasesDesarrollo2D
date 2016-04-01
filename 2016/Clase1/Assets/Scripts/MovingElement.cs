73ab50f3f3e91efd2a9cf8135cba2407669573e6
Alumno-J <Alumno-J>
2016-03-31 22:49:35 -0500

    clase12

---
+++ 2016/Clase1/Assets/Scripts/MovingElement.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovingElement : MonoBehaviour {
+
+	// Use this for initialization
+	public float speed;
+	Vector3 limit;
+	void Start () {
+		limit = 
+			GameObject.Find ("limit").
+			transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 position = transform.position;
+		position.y -= speed*Time.deltaTime;
+		transform.position = position;
+		if(limit.y>transform.position.y){
+			Destroy (gameObject);
+		}
+	}
+}
---
+++ 2016/Clase1/Assets/Scripts/Spawner.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spawner : MonoBehaviour {
+
+	// Use this for initialization
+	public GameObject[] elements;
+	float elapsed;
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {		
+		elapsed += Time.deltaTime;
+		if(elapsed >= 1){
+			elapsed = 0;
+			generateElement ();
+		}
+	}
+
+	void generateElement(){
+		int index = Random.Range (0,
+				elements.Length-1);
+		GameObject element = elements [index];
+		Instantiate (element, 
+			new Vector3 (Random.Range(-5f,5f),
+				transform.position.y
+					, 0), 
+			transform.rotation);
+	}
+}
